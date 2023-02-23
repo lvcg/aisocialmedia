678fac20c3885d5567624a2849d45b7a004ceb37
unknown <flowfxdesignsonline@gmail.com>
2023-02-23 15:47:07 -0600

    connected backend to onrender

--- .history/client/src/pages/CreatePost_20230223153206.jsx
+++ .history/client/src/pages/CreatePost_20230223153206.jsx
@@ -58,7 +58,7 @@ const CreatePost = () => {
       setLoading(true);
 
       try {
-        const response = await fetch ('https://ai-social-media.onrender.com/api/v1/post', {
+        const response = await fetch ('http://localhost:8080/api/v1/post', {
           method: 'POST',
           headers: {
             'Content-Type': 'application/json',  
